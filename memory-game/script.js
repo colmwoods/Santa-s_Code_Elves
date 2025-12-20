f52ebbdf67069d88c6e9e78f0e1534b13dff891d
Colm Woods <colmwoods13@gmail.com>
2025-12-20 12:28:57 +0000

    Add my shuffle function to shuffle cards random

--- memory-game/script.js
+++ memory-game/script.js
@@ -4,3 +4,10 @@ const restartBtn = document.getElementById("restart");
 let firstCard = null;
 let secondCard = null;
 let lockBoard = false;
+
+function shuffle() {
+    cards.forEach(card => {
+        const randomPos = Math.floor(Math.random() * cards.length);
+        card.style.order = randomPos;
+    });
+}
